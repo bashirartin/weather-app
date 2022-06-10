2bec38679b9e0ef099f3ddd20650542fbbcf2f5a
Bashir Shahbakhsh <bashirartin@gmail.com>
2022-06-10 14:25:21 +0430

    second upload

---
+++ README.md
@@ -0,0 +1,3 @@
+weatherstack api access key = cbc9cf1fb24400709300edaef8b6c353
+base url: http://api.weatherstack.com/
+
--- app.js
+++ app.js
@@ -1,8 +1,12 @@
-console.log('Starting')
+const request = require('request')
 
-setTimeout(function () {
-    console.log('2 Second Timer')
-}, 2000);
+const url = 'http://api.weatherstack.com/current?access_key=cbc9cf1fb24400709300edaef8b6c353&query=37.8267,-122.4233&units=s'
 
 
-console.log('Stopping')
+request({url: url, json: true}, (error, Response) => {
+    const temp = Response.body.current.temperature
+    const precip = Response.body.current.precip
+    const feelsLike = Response.body.current.feelslike
+    console.log(Response.body.current.weather_descriptions[0]+". It's currently "+temp+" degrees out and It's feels like "+feelsLike+" degrees. There's a "+
+    precip+"% chance of rain.")
+})
